25ee0aa3884962db7633897f52b7c98216daedf7
Cheng Bo-Yan <markxp2002@gmail.com>
2026-03-13 22:13:10 +0800

    add usage example

---
+++ example/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eux
+
+# modify php-fpm pool (www.conf) settings at instance starting up
+# we need our pool to respond health check to fastcgi tool.
+POOL_CONF='/usr/local/etc/php-fpm.d/www.conf'
+if [ -f "$POOL_CONF" ]; then
+	sed -i \
+		-e "s|^;pm.status_path = /status$|^pm.status_path = /status$|g" \ 
+		"$POOL_CONF"
+	echo "[PHP-FPM.conf] successfully swap environment variables" 
+else
+	echo "[PHP-FPM.conf] www.conf does not exist. Skip to modify" 
+fi
+
+HEALTH_SERVER='/usr/local/bin/health-server'
+CHECK_SCRIPT='/usr/local/bin/php-fpm-healthcheck'
+if [ -x "$HEALTH_SERVER" -a -x "$CHECK_SCRIPT" ]; then
+	health-server & 
+else
+	echo 'health-server or php-fpm-healthcheck does not exist. liveness probe is unavaliable.'
+fi
+exec docker-php-entrypoint "$@"
